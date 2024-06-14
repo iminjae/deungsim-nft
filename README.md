c496415abf25113c4fcbba267437ef8db65ad0d8
iminjae <iminjae.github.com>
2024-06-14 09:35:31 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# deungsim nft
---
+++ vite/src/App.tsx
@@ -0,0 +1,8 @@
+import { Flex } from "@chakra-ui/react";
+import { FC } from "react";
+
+const App: FC = () => {
+  return <Flex>Hello, React!</Flex>;
+};
+
+export default App;
